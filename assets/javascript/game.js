3881d56f758fa0acc3c268efdd15c7da682ba859
Amy Lumpkin <amylumpkin@amys-mbp.attlocal.net>
2017-12-19 15:03:13 -0500

    more tweaks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,40 @@
-$(document).ready( function() {
+//document ready function waits for all other aspects of app to be ready before triggering JS
+$(document).ready(function() {
+    reset();   //calls the reset function when page opens
 
+//telling program what's coming next, a function, name to reference later, (placeholder)
+//no value yet
 function getRandomNumber(min, max) {
+    //any function that returns a value, when it resolves, becomes that value.
     return Math.floor(Math.random() * (max - min + 1)) + min;
+                                    //number under 120     //number at least 19           
 }
-var targetNumber = getRandomNumber(19, 120);
-console.log(targetNumber);
-$("#number-to-guess").text(targetNumber);
-var randomNumber1 = getRandomNumber(1,12);
-var randomNumber2 = getRandomNumber(1,12);
-var randomNumber3 = getRandomNumber(1,12);
-var randomNumber4 = getRandomNumber(1,12);
 
-console.log(randomNumber1);
-console.log(randomNumber2);
-console.log(randomNumber3);
-console.log(randomNumber4);
 
 //setting starting values
 var sum = 0; 
 var wins = 0;
 var losses = 0;
 // wins and losses counter
-$("#numberWins").text(wins);
+//this makes values display on browser
+// ( HTML ).displayed value - "Wins: "(the thing being displayed - the value, number)
+$("#numberWins").text(wins);   //'number Wins' is now equal to a value
 $("#numberLosses").text(losses);
 
 //resets the game
-function reset() {
-    targetNumber = getRandomNumber(18, 120);
-    $("#number-to-guess").text(targetNumber);
+function reset() {    //defines the reset function
+    targetNumber = getRandomNumber(18, 120); //targetNumber is equal to the solution
+
+    //grabbing html and setting text content.
+    $("#number-to-guess").text(targetNumber);//that's why we get an interger here
     console.log(targetNumber);
+
+    //running the function, they don't actually link to crystals yet.
     randomNumber1 = getRandomNumber(1,12);
     randomNumber2 = getRandomNumber(1,12);
     randomNumber3 = getRandomNumber(1,12);
     randomNumber4 = getRandomNumber(1,12);
+
     console.log(randomNumber1);
     console.log(randomNumber2);
     console.log(randomNumber3);
@@ -46,7 +48,7 @@ function win() {
     alert("You won!");
     wins++; 
     $("#numberWins").text(wins);
-    reset();
+    reset(); //calling the reset function here, 
 }
 //function to count losses
 function lose() {
@@ -62,13 +64,13 @@ $("#crystal1").on("click", function() {
     $("#finalSum").text(sum); 
     
     if (sum == targetNumber) { 
-        win();
+        win();  //calling the win function
      }
-    else if ( sum > targetNumber) { 
+    else if (sum > targetNumber) { 
          lose();
      }   
 });  
-$("#crystal2").on ("click", function() {
+$("#crystal2").on ("click", function() {   //all four of these functions can be one. (DRY)
     sum += randomNumber2;
     console.log(sum);
     $("#finalSum").text(sum); 
@@ -88,7 +90,7 @@ $("#crystal3").on("click", function() {
     if (sum == targetNumber) {
         win();
     }
-    else if ( sum > targetNumber) {
+    else if (sum > targetNumber) {
         lose();
     }   
 });  
@@ -100,7 +102,7 @@ $("#crystal4").on("click", function() {
     if (sum == targetNumber) {
         win();
     }
-    else if ( sum > targetNumber) {
+    else if (sum > targetNumber) {
         lose();
     }   
 });  
