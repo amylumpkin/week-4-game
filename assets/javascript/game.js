612976c2d8944374f599bf7f4d8c79cbbb08dda6
Amy Lumpkin <amylumpkin@Amys-MacBook-Pro.local>
2017-12-20 15:42:53 -0500

    final-for now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,16 @@
-//document ready function waits for all other aspects of app to be ready before triggering JS
+//document ready function for jquery waits for all other aspects of app to be ready (HTML, CSS & JS) before triggering
 $(document).ready(function() {
-    reset();   //calls the reset function when page opens
+    reset();   //calls the reset function (defined below) when page opens
 
 //telling program what's coming next, a function, name to reference later, (placeholder)
-//no value yet
+//no value yet, just creating the function
+
+//function   name        placeholder
 function getRandomNumber(min, max) {
+
     //any function that returns a value, when it resolves, becomes that value.
     return Math.floor(Math.random() * (max - min + 1)) + min;
-                                    //number under 120     //number at least 19           
+                                    //number under 120     //number at least 18           
 }
 
 
@@ -16,12 +19,12 @@ var total = 0;
 var wins = 0;
 var losses = 0;
 // wins and losses counter
-//this makes values display on browser
-// ( HTML ).displayed value - "Wins: "(the thing being displayed - the value, number)
-$("#numberWins").text(wins);   //'number Wins' is now equal to a value
+//this makes values display on browser within the HTML
+// ( HTML ).displayed value - Wins: (the thing being displayed - the value, number)
+$("#numberWins").text(wins);   //'numberWins' is now equal to a value
 $("#numberLosses").text(losses);
 
-//defining the reset function
+//defining the reset function - chooses randam number to guess, numbers for crytals, and sets total number back to 0.
 function reset() {   
     targetNumber = getRandomNumber(18, 120); //targetNumber is equal to the solution
 
@@ -31,6 +34,7 @@ function reset() {
 
     //running the function, they don't actually link to crystals yet.
     randomNumber1 = getRandomNumber(1,12);
+
     randomNumber2 = getRandomNumber(1,12);
     randomNumber3 = getRandomNumber(1,12);
     randomNumber4 = getRandomNumber(1,12);
@@ -58,10 +62,11 @@ function lose() {
     reset();
 }
 //click functions for crystals
-$("#crystal1").on("click", function() {      //all four of these click functions can be one. (DRY)
+
+$("#crystal1").on("click", function() {      //all four of these click functions can be combined into one, I just know it!!!. (DRY)
     total += randomNumber1;   //concatenates a string (links things together)
     console.log(total);
-    $("#finalTotal").text(total); // $(html id or class).text(a value)
+    $("#finalTotal").text(total); // $(html id or class - this grabs the html).whatever text is in the html(a value)
     
     if (total == targetNumber) { 
         win();  //calling the win function
@@ -69,6 +74,8 @@ $("#crystal1").on("click", function() {      //all four of these click functions
     else if (total > targetNumber) { 
          lose(); //calling the lose function
      }   
+
+//these are essentially the same as crystal 1...   
 }); 
 $("#crystal2").on("click", function() {   
     total += randomNumber2;       
